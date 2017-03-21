a8c308d83027779fd3d49487b0ca6c6316c13fda
TAKEHITO Koshimizu <takehito.koshimizu@glpgs.com>
2017-03-21 23:14:22 +0900

    add targets for mac and watch

---
+++ HashAlgorithmMac/HashAlgorithmMac.h
@@ -0,0 +1,19 @@
+//
+//  HashAlgorithmMac.h
+//  HashAlgorithmMac
+//
+//  Created by Takehito Koshimizu on 2017/03/21.
+//  Copyright © 2017年 Takehito Koshimizu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HashAlgorithmMac.
+FOUNDATION_EXPORT double HashAlgorithmMacVersionNumber;
+
+//! Project version string for HashAlgorithmMac.
+FOUNDATION_EXPORT const unsigned char HashAlgorithmMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HashAlgorithmMac/PublicHeader.h>
+
+
---
+++ HashAlgorithmWatch/HashAlgorithmWatch.h
@@ -0,0 +1,19 @@
+//
+//  HashAlgorithmWatch.h
+//  HashAlgorithmWatch
+//
+//  Created by Takehito Koshimizu on 2017/03/21.
+//  Copyright © 2017年 Takehito Koshimizu. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for HashAlgorithmWatch.
+FOUNDATION_EXPORT double HashAlgorithmWatchVersionNumber;
+
+//! Project version string for HashAlgorithmWatch.
+FOUNDATION_EXPORT const unsigned char HashAlgorithmWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HashAlgorithmWatch/PublicHeader.h>
+
+
